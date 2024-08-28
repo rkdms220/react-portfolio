9b8aec1b4b6df69bda504076151d031c015811a5
rkdms220 <gggom1213@naver.com>
2024-08-28 21:18:08 +0900

    edit

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+  return (
+    <div id="playerStats">
+      <h1>SEONG GAEUN LEVEL 22</h1>
+      <div id="playerStats__lines">
+        <span id="playerStats__lines__thick"></span>
+        <span id="playerStats__lines__thin"></span>
+      </div>
+      <h2>Frontend Developer</h2>
+    </div>
+  );
+};
+
+export default PlayerStats;
